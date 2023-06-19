769c37ed7f7a42d567eb9cedb11bffd7ce818b6d
Kamilbur <kamil.burkiewicz@gmail.com>
2023-06-19 22:09:09 +0200

    Overwrite existing parameter

--- tf/ssm.tf
+++ tf/ssm.tf
@@ -2,5 +2,6 @@ resource "aws_ssm_parameter" "processing_bucket" {
   name       = "processing_bucket"
   type       = "String"
   value      = aws_s3_bucket.processing_bucket.bucket
+  overwrite = true
   depends_on = [module.root]
 }
